62fecc9051cb3fa4d1c47cbb4b29a9b2a4b70a7b
Rafa C <rafasantcont@yahoo.com.br>
2016-08-04 20:19:52 -0300

    Buscar jogos do brasileiro entre 2012 e 2016 ou passar ano(s) como argumento.

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 'use strict';
 
 const crawler    = require('./lib/crawler');
-const parser = require('./lib/parser');
-const cheerio    = require('cheerio');
+const htmlParser = require('./lib/parser');
 const fs         = require('fs');
-const url        = "http://www.cbf.com.br/competicoes/brasileiro-serie-a/tabela/2016";
 
-crawler.get(url)
-  .then(function(data) {
-    let jogos = parser.getJogos(data);
+let anos = [ 2012, 2013, 2014, 2015, 2016];
 
-    let file = fs.createWriteStream('data/brasileiro.json');
+if (process.argv.length > 2)
+  anos = process.argv.slice(2);
 
-    file.on('error', function(err) { /* error handling */ });
-    jogos.forEach(function(jogo) {
-      file.write(JSON.stringify(jogo) + '\n');
-    });
-    file.end();
+Promise.all(anos.map(ano => { return crawler.get(ano)} ))
+  .then(function (data) {
+    data.forEach(function (elem) {
+      let arquivo = fs.createWriteStream('data/brasileiro' + elem.ano + '.json');
 
+      arquivo.on('error', function(err) { /* error handling */ });
+      elem.data('.tabela-jogos').find('.row.full-game').each(function (i) {
+          const gameInfo = elem.data(this).parent().parent().children('.col-md-12.col-lg-12');
+          arquivo.write(JSON.stringify(htmlParser.jogo(gameInfo)) + '\n');
+      })
+      arquivo.end();
+
+      console.log(arquivo.path);
+    })
   })
-  .catch(
-    (err) => console.error(err)
-  );
