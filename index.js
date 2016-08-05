f4d71fa9794077d8ae67463799041ae328fe0f58
Rafa C <rafasantcont@yahoo.com.br>
2016-08-04 23:02:44 -0300

    Adicionar atributo rodada.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const crawler    = require('./lib/crawler');
-const htmlParser = require('./lib/parser');
+const parse      = require('./lib/parse');
 const fs         = require('fs');
 
 let anos = [ 2012, 2013, 2014, 2015, 2016];
@@ -16,8 +16,7 @@ Promise.all(anos.map(ano => { return crawler.get(ano)} ))
 
       arquivo.on('error', function(err) { /* error handling */ });
       elem.data('.tabela-jogos').find('.row.full-game').each(function (i) {
-          const gameInfo = elem.data(this).parent().parent().children('.col-md-12.col-lg-12');
-          arquivo.write(JSON.stringify(htmlParser.jogo(gameInfo)) + '\n');
+          arquivo.write(JSON.stringify(parse.jogoCampeonatoBrasileiro(elem.data(this))) + '\n');
       })
       arquivo.end();
 
