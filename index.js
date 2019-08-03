5a5946de58ff71dee2955ab4b790da1a857b8369
Rafa C <rafasantcont@yahoo.com.br>
2019-08-03 18:05:54 -0300

    Atualizar crawler para pegar dados da nova página da CBF

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
+
 const crawler    = require('./lib/crawler');
-const parse      = require('./lib/parse');
+let Parse = require('./lib/parse'), parse = new Parse();
 const fs         = require('fs');
 
-let anos = [ 2012, 2013, 2014, 2015, 2016];
+let anos = [ 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 ];
 
 if (process.argv.length > 2)
   anos = process.argv.slice(2);
@@ -19,11 +20,17 @@ Promise.all(anos.map(ano => { return crawler.get(ano)} ))
       let arquivo = fs.createWriteStream('data/brasileiro' + elem.ano + '.json');
 
       arquivo.on('error', function(err) { /* error handling */ });
-      elem.data('.tabela-jogos').find('.row.full-game').each(function (i) {
-          arquivo.write(JSON.stringify(parse.jogoCampeonatoBrasileiro(elem.data(this))) + '\n');
+      parse.todasAsRodadas(elem.data).each(function (i) {
+        let rodada = parse.numeroRodada(elem.data(this));
+        let jogos = parse.todosOsJogos(elem.data(this));
+        
+        jogos.each(function (i) {
+          var detalhes = parse.detalhesJogo(elem.data(this), rodada);
+          arquivo.write(JSON.stringify(detalhes) +'\n');
+        })
       })
       arquivo.end();
 
       console.log(arquivo.path);
     })
-  })
+})
